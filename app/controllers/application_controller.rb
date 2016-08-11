68ee8fc2e41064cde7eca42ab07764710f3e7edc
Erin Morrissey <erinmorrissey@gmail.com>
2016-08-11 20:46:48 +0000

    Configure public/404.html page to display for status not_found errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   private
 
   def render_not_found(status=:not_found)
-    render text: '#{status.to_s.titleize}', status: status
+    #render text: '#{status.to_s.titleize}', status: status
+    render file: 'public/404.html', status: status
   end
 end
